9b8f86da02ab34a5fad11d192f9aacaa1c67d7ed
matheusjesse <matheus_jesse@hotmail.com>
2022-12-23 20:43:17 -0300

    req 01

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if type(target_time) is not int
+        return None
+
+    students_count = 0
+
+    for entry, exit in permanence_period:
+        if type(entry) is not int or type(exit) is not int:
+            return None
+
+        if entry <= target_time <= exit:
+            students_count += 1
+    
+    return students_count
