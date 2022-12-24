97ddc92bf12b404eb5354d7c3a91cc4f4f3cb1bb
matheusjesse <matheus_jesse@hotmail.com>
2022-12-23 21:12:36 -0300

    req 02

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,14 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    message = "AABBCC"
+    assert encrypt_message(message, -1) == "CCBBAA"
+    
+    assert encrypt_message(message, 3) == "BAA_CCB"
+    
+    assert encrypt_message(message, 4) == "AC_CBBA"
+    
+    with pytest.raises(TypeError, match="tipo inválido para key"):
+        encrypt_message(message, "string")
