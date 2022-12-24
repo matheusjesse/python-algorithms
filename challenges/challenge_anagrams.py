82956409e18c52158f60f9905abe6f732bc9ffcb
matheusjesse <matheus_jesse@hotmail.com>
2022-12-24 01:28:42 -0300

    req04

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,3 +1,19 @@
 def is_anagram(first_string, second_string):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    first = alphabetical_order(first_string)
+    second = alphabetical_order(second_string)
+    check_string = first == second
+
+    return (first, second, check_string)
+
+
+def alphabetical_order(string):
+    lowered_string = string.lower()
+    unsorted = list(lowered_string.strip())
+    sort_string = ""
+
+    while unsorted:
+        smallest = min(unsorted)
+        sort_string += smallest
+        unsorted.pop(unsorted.index(smallest))
+
+    return sort_string
