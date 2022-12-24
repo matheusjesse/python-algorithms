c2b236127b2cc23a34edb3dad1de40993e043777
matheusjesse <matheus_jesse@hotmail.com>
2022-12-24 04:51:59 -0300

    req 06

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,7 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not word:
+        return False
+# inversão palavra
+# https://pt.stackoverflow.com/questions/401489/inverter-as-palavras-de
+# -uma-frase-mantendo-a-ordem-delas-na-frase
+    return word[::-1] == word
