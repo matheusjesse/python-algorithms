9053c5ed35b96ebd4ab937d2e6a558f6de7a378d
matheusjesse <matheus_jesse@hotmail.com>
2022-12-24 03:19:27 -0300

    req05

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,14 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or len(nums) <= 1 or type(nums) is not list:
+        return False
+
+    newlist = []
+    dupli = []
+
+    for num in nums:
+        if num not in newlist:
+            newlist.append(num)
+        else:
+            return num
+
+    return False
