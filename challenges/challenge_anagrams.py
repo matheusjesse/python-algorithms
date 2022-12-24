3b299581e9efc1d6cd5a83c239ae75684030c5d5
matheusjesse <matheus_jesse@hotmail.com>
2022-12-24 02:39:07 -0300

    req 04 refactor

--- challenges/challenge_anagrams.py
+++ challenges/challenge_anagrams.py
@@ -1,17 +1,15 @@
 def is_anagram(first_string, second_string):
-    first = alphabetical_order(first_string)
-    second = alphabetical_order(second_string)
+    first = alphabetical_order(list(first_string.strip()))
+    second = alphabetical_order(list(second_string.strip()))
 
     return (first, second, first == second)
 
 
-def alphabetical_order(string):
-    lowered_string = string.lower()
-    sort_string = ""
-
-    while lowered_string:
-        smallest = min(lowered_string)
-        sort_string += smallest
-        lowered_string = lowered_string.replace(smallest, '', 1)
-
-    return sort_string
+def alphabetical_order(string_list):
+    if not string_list:
+        return []
+    return (alphabetical_order(
+            [x for x in string_list[1:] if x < string_list[0]])
+            + [string_list[0]] +
+            alphabetical_order(
+            [x for x in string_list[1:] if x >= string_list[0]]))
