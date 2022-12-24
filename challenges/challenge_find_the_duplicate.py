d583a4f161cc29400214d7fb77c172fcfcd3d627
matheusjesse <matheus_jesse@hotmail.com>
2022-12-24 04:28:02 -0300

    fix

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -2,13 +2,13 @@ def find_duplicate(nums):
     if nums:
         sort_list = sorted(nums)
 
-        newlist = []
+        new_list = []
 
         for num in sort_list:
             if type(num) is not int or num < 1:
                 return False
-            if num not in newlist:
-                newlist.append(num)
+            if num not in new_list:
+                new_list.append(num)
             else:
                 return num
 
